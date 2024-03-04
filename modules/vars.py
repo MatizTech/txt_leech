c8ccb14de0a7dfa71466bf8cc178fea4cef86dd3
Txtitsy <162138904+Txtitsy@users.noreply.github.com>
2024-03-04 19:16:52 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "20828011"
+api_hash = "26da1e1a4b489dd65c3616905c4a9090"
+bot_token = "7099225640:AAEpuBkt67YB95cQ9f1rv7RYggUVrKYzo1I"
