0cde781cfd85c389fc601d7965d08c0feff9cc0f
Amanpathak1 <155675919+Amanpathak1@users.noreply.github.com>
2024-03-23 08:07:37 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "22253680"
+api_hash = "4fc4c646519fe43891bebd1449744b7e"
+bot_token = "7150544151:AAEMGUmGH0vJj1yeNZ7By3Nsj_p4Q6-UFDA"
