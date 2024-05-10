7dcb96093afc924b7e433d59d53e984ae9126076
Liveallclass <154664537+livecallclass@users.noreply.github.com>
2024-05-11 01:20:14 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "23359785"
+api_hash = "d1bb3e741e60fc0c54cbdb263684f728"
+bot_token = "6738412366:AAFZnt-eNjdl6zu0gZw4lEqdrOJr7YUMGjM"
